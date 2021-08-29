91048642bdd35c57f42b0325c263b4570afb3dd1
SaminYeaser <saminyeaser1@gmail.com>
2021-08-29 20:08:26 +0600

    progress bar animation done

--- lib/pages/components/body.dart
+++ lib/pages/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizappmath/constants.dart';
+import 'package:quizappmath/pages/components/progresbar.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
 class Body extends StatelessWidget {
@@ -20,39 +21,8 @@ class Body extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
             child: Column(
               children: [
-                Container(
-                  width: double.infinity,
-                  height: 35,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xFF3F4768), width: 3),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Stack(
-                    children: [
-                      LayoutBuilder(
-                        builder: (context, constraints) => Container(
-                          width: constraints.maxWidth * .5,
-                          decoration: BoxDecoration(
-                              gradient: kPrimaryGradient,
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                      ),
-                      Positioned.fill(
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: kDefaultPadding / 2),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('18 Sec'),
-                              WebsafeSvg.asset("assets/icons/clock.svg")
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                ProgressBar(),
+
               ],
             ),
           ),
---
+++ lib/pages/components/progresbar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:quizappmath/controllers/questionController.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+import '../../constants.dart';
+
+class ProgressBar extends StatelessWidget {
+  const ProgressBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 35,
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xFF3F4768), width: 3),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: GetBuilder<QuestionController>(
+        init: QuestionController(),
+        builder: (controller) {
+          print(controller.animation!.value);
+          return Stack(
+            children: [
+              LayoutBuilder(
+                builder: (context, constraints) => Container(
+                  width: constraints.maxWidth * controller.animation!.value,
+                  decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+              Positioned.fill(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: kDefaultPadding / 2),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('${(controller.animation!.value * 60).round()} Sec'),
+                      WebsafeSvg.asset("assets/icons/clock.svg")
+                    ],
+                  ),
+                ),
+              )
+            ],
+          );
+        }
+      ),
+    );
+  }
+}
